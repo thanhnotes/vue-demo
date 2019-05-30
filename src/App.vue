f86e8568ea2e03116c31767c7ab0f2d2b295daab
Thanh Huynh <thanhhuynh@azwebplus.com>
2019-05-31 05:24:46 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class='amber--text'>Todo</span>
+        <span class="font-weight-light">-List</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://google.com"
+        target="_blank"
+      >
+        <span class="mr-2">Google Phát!</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p
+      class="orange white--text"
+    >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi totam ut maiores minus molestiae. Quae accusamus amet sunt nisi, quos excepturi vitae minima velit obcaecati omnis temporibus aut, sequi dolore!</p>
+    <p
+      class="pink lighten-4 yellow--text"
+    >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam recusandae dicta voluptatum dolor ipsum laboriosam placeat doloremque, soluta eum temporibus hic ex odit non officia nemo sit culpa esse corporis!</p>
+    <p
+      class="purple"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa, reiciendis nesciunt voluptatibus eveniet voluptates temporibus? Nisi non impedit fuga ex, soluta at rerum deserunt sit accusantium nostrum veritatis ab!</p>
+    <p class="display-4">Massive Display</p>
+    <h4 class='display-1 font-weight-light font-italic'>Smaller Display</h4>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
